eb5c5a8edea27caa8a64867ab14550520bd383be
Tin Pham <tinpham@Tins-MacBook-Pro.local>
2020-11-22 12:52:34 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+     return (
+         <div className='App'>
+             <h1>HELLO, WORLD!</h1>
+             <h2>Web App</h2>
+             <h3>Gabe DeGuzman - Team Lead (github ID: gedeguz007)</h3>
+             <h3>Tin Pham (github ID: tinpham5614)</h3>
+             <h3>YingHeng He (github ID: SEA-Hank)</h3>
+             <h3>Patrick Tensen (github ID: Squirrel10)</h3>
+             <h3>Omurbek Mamasaliev (github ID: omar-devops)</h3>
+             
+         </div>
+     );
+}
+
+export default App;
